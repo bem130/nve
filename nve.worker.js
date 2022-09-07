e8e591a7e94a446a13de38f322b25720ca5c1e7e
bem130 <haruki070617@gmail.com>
2022-09-07 21:02:29 +0900

    change a little

--- nve.worker.js
+++ nve.worker.js
@@ -1,107 +1,230 @@
-let dispsize = [1280,720]
-
-
-let Mouse= {loc:[0,0]}
-self.addEventListener("message", (e) => {
-    if (e.data[0]=="MouseLoc") {
-        Mouse.loc = e.data[1]
+class NVM {
+    #prog;#imme;#memr;#istr;#ostr;#ispt;#ospt;#regi; // 宣言
+    constructor(program,args=[0]) { // 初期化
+        let tbr = this.tbyte(program);
+        this.#prog = tbr[0]; // プログラム
+        this.#imme = tbr[1]; // 即値
+        this.#memr = new Uint8Array(1024); // データ
+        this.#istr = new Uint8Array(args); // 標準入力
+        this.#ostr = new Uint8Array(1024); // 標準出力
+        this.#ispt = 0; // 標準入力ポインタ
+        this.#ospt = 0; // 標準出力ポインタ
+        this.#regi = [0,0,0,0,0]; // i p x a b
     }
-});
+    next() { // 一つの命令を実行する
+        if (this.endRunning()) {console.warn("end runnning")}
+        switch (this.#prog[this.#regi[0]]) {
+            case 0: // get
+                this.#regi[2] = this.#istr[this.#ispt];
+                this.#ispt++;
+            break;
+            case 1: // outx
+                this.#ostr[this.#ospt] = this.#regi[2];
+                this.#ospt++;
+            break;
+            case 2: // outm
+                this.#ostr[this.#ospt] = this.#memr[this.#regi[1]];
+                this.#ospt++;
+            break;
 
-class Draw {
-    constructor() {
-        this.dpds = new Uint8ClampedArray(dispsize[0]*dispsize[1]*3).fill(0);
-    }
-    dot(x,y,rgb) {
-        let idx = (y*dispsize[0]+x)*3;
-        this.dpds[idx+0] = rgb[0]
-        this.dpds[idx+1] = rgb[1]
-        this.dpds[idx+2] = rgb[2]
-    }
-    update() {
-        let x = dispsize[0]
-        let y = dispsize[1]
-        let iarr = new Uint8ClampedArray(x*y*4).fill(255);
-        for (let iy = 0; iy < y; iy++) {
-            for (let ix = 0; ix < x; ix++) {
-                let iarridx = (iy*x+ix)*4;
-                let dpdsidx = (iy*x+ix)*3;
-                iarr[iarridx+0] = this.dpds[dpdsidx+0];
-                iarr[iarridx+1] = this.dpds[dpdsidx+1];
-                iarr[iarridx+2] = this.dpds[dpdsidx+2];
-            }
+            case 3: // equal
+                this.#regi[2] = this.#regi[3]==this.#regi[4];
+            break;
+            case 4: // less
+                this.#regi[2] = this.#regi[3]<this.#regi[4];
+            break;
+            case 5: // greater
+                this.#regi[2] = this.#regi[3]>this.#regi[4];
+            break;
+            case 6: // eqgoto
+                if (this.#regi[2]==0) {
+                    this.#regi[0] = this.#imme[this.#regi[0]];
+                    this.#regi[0]-=2;
+                }
+            break;
+            case 7: // uneqgoto
+                if (this.#regi[2]!=0) {
+                    this.#regi[0] = this.#imme[this.#regi[0]];
+                    this.#regi[0]-=2;
+                }
+            break;
+            case 8: // goto
+                this.#regi[0] = this.#imme[this.#regi[0]];
+                this.#regi[0]-=2;
+            break;
+
+            case 9: // movpx
+                this.#regi[1] = this.#regi[2]
+            break;
+            case 10: // movpv
+                this.#regi[1] = this.#imme[this.#regi[0]]
+            break;
+            case 11: // movmx
+                this.#memr[this.#regi[1]] = this.#regi[2]
+            break;
+            case 12: // movmi
+                this.#memr[this.#regi[1]] = this.#imme[this.#regi[0]]
+            break;
+
+            case 13: // movam
+                this.#regi[3] = this.#memr[this.#regi[1]]
+            break;
+            case 14: // movbm
+                this.#regi[4] = this.#memr[this.#regi[1]]
+            break;
+            case 15: // movxm
+                this.#regi[2] = this.#memr[this.#regi[1]]
+            break;
+            case 16: // movai
+                this.#regi[3] = this.#imme[this.#regi[0]]
+            break;
+            case 17: // movbi
+                this.#regi[4] = this.#imme[this.#regi[0]]
+            break;
+            case 18: // movxi
+                this.#regi[2] = this.#imme[this.#regi[0]]
+            break;
+            case 29: // movax
+                this.#regi[3] = this.#regi[2]
+            break;
+            case 20: // movbx
+                this.#regi[4] = this.#regi[2]
+            break;
+
+            case 21: // add
+                this.#regi[2] = this.#regi[3]+this.#regi[4];
+            break;
+            case 22: // sub
+                this.#regi[2] = this.#regi[3]-this.#regi[4];
+            break;
+            case 23: // mul
+                this.#regi[2] = this.#regi[3]*this.#regi[4];
+            break;
+
+            case 24: // and
+                this.#regi[2] = Boolean(this.#regi[3])&&Boolean(this.#regi[4]);
+            break;
+            case 25: // or
+            this.#regi[2] = Boolean(this.#regi[3])||Boolean(this.#regi[4]);
+            break;
+            case 26: // xor
+            this.#regi[2] = Boolean(this.#regi[3])^Boolean(this.#regi[4]);
+            break;
+            case 27: // not
+            this.#regi[2] = !Boolean(this.#regi[3]);
+            break;
+
+            case 28: // inc
+                this.#regi[2] = this.#regi[3]+1;
+            break;
+            case 39: // dec
+                this.#regi[2] = this.#regi[3]-1;
+            break;
+
+            case 30: // rshift
+                this.#regi[2] = this.#regi[3]>>1;
+            break;
+            case 31: // lshift
+                this.#regi[2] = this.#regi[3]<<1;
+            break;
+
+            default:
+            break;
         }
-        self.postMessage(["Out",iarr,dispsize]);
+        this.#regi[0]++;
+        return this;
     }
+    runall() { // 最後まで命令を実行する(最大10000)
+        let cnt = 0;
+        while (cnt<10000&&!this.endRunning()) {cnt++;this.next();}
+        return this;
+    }
+    tbyte(program) { // テキストを数値の配列に変換する
+        let icnt = 0;
+        // m memory; i immeddiate; p memory-pointer; x result; a,b args;
+        let ins = ["get","outx","outm","equal","less","greater","eqgoto","uneqgoto","goto","movpx","movpi","movmx","movmv","movam","movbm","movxm","movai","movbi","movxi","movax","movbx","add","sub","mul","and","or","xor","not","inc","dec","rshift","lshift"];
+        let lines = program.replace(/\r/,"").split("\n");
+        let tlss = [];
+        for (let l=0;l<lines.length;l++) {
+            let s;
+            for (s=0;s<lines[l].length;s++) {
+                if (lines[l][s]!=" ") {break;}
+            }
+            let c;
+            for (c=0;c<lines[l].length;c++) {
+                if (lines[l][c]==";") {break;}
+            }
+            let tl = lines[l].slice(s,c)
+            if (tl.length==0) {continue;}
+            let tls = tl.split(" ");
+            tlss.push(tls);
+            if (ins.indexOf(tls[0])==-1) {continue;}
+            icnt++;
+            if (tls[tls.length-1]==":") {icnt--;}
+        }
+        let prog = new Uint8ClampedArray(icnt);
+        let imme = new Uint8ClampedArray(icnt);
+        let ic = 0;
+        let labeladr = {};
+        for (let i=0;i<tlss.length;i++) {
+            ic++;
+            if (tlss[i][0][tlss[i][0].length-1]==":") {
+                labeladr[tlss[i][0].slice(0,tlss[i][0].length-1)] = ic;
+                ic--;
+            }
+            if (ins.indexOf(tlss[i][0])==-1) {ic--;continue;}
+        }
+        ic = 0;
+        for (let i=0;i<tlss.length;i++) {
+            if (tlss[i][0][tlss[i][0].length-1]==":") {continue;}
+            if (ins.indexOf(tlss[i][0])==-1) {continue;}
+            prog[ic] = ins.indexOf(tlss[i][0]);
+            if (["goto","eqgoto","uneqgoto"].indexOf(tlss[i][0])!=-1) {
+                imme[ic] = labeladr[tlss[i][1]];
+            }
+            else if (tlss[i].length>0) {
+                imme[ic] = parseInt(tlss[i][1]);
+            }
+            ic++;
+        }
+        return [prog,imme];
+    }
+    endRunning() {if(this.#regi[0]>=this.#prog.length){return true};return false;}
+    nextRead() {if(this.#prog[this.#regi[0]]==5){return true};return false;}
+    getOut(format="utf-8") {return (new TextDecoder(format)).decode(new Uint8Array(this.#ostr.slice(0,this.#ospt)));} // 出力をテキストで取得
+    getBinOut() {return new Uint8Array(this.#ostr.slice(0,this.#ospt))} // 出力を配列で取得
+    getProg() {return this.#prog}
+    getImme() {return this.#imme}
+    getData() {return this.#memr}
+    getIptr() {return this.#regi[0]}
+    getDptr() {return this.#regi[1]}
+    getRegi() {return this.#regi}
 }
 
-draw = new Draw()
+code = `
+goto main
+main:
+    movai 1 ; set a to 1
+    movbi 1 ; set b to 1
+    add     ; a+b (1+1)
+    movpi 0 ; set mem-address to 0
+    movmx
 
+    movai 7 ; set a to 7
+    movbi 9 ; set b to 9
+    mul     ; a*b (1+1)
+    movpi 1 ; set mem-address to 1
+    movmx
 
-cursorImg = [
-    [1,0,0,0,0,0,0,0,0,0],
-    [1,1,0,0,0,0,0,0,0,0],
-    [1,2,1,0,0,0,0,0,0,0],
-    [1,2,2,1,0,0,0,0,0,0],
-    [1,2,2,2,1,0,0,0,0,0],
-    [1,2,2,2,2,1,0,0,0,0],
-    [1,2,2,2,2,2,1,0,0,0],
-    [1,2,2,2,2,2,2,1,0,0],
-    [1,2,2,2,2,2,2,2,1,0],
-    [1,1,2,2,2,2,2,2,1,1],
-]
+    movam   ; set a to mem-address 1
+    movpi 0
+    movbm   ; set b to get mem-address 0
+    add     ; a+b (2+63)
 
-NeknajImg = [
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0],
-    [0,1,1,0,0,1,0,0,0,1,1,0,1,1,0,0,0,1,0,0,0,1,0],
-    [0,1,0,1,0,0,1,1,0,1,0,0,1,0,1,0,1,0,1,0,0,1,0],
-    [0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,0,0],
-]
+    outx    ; output the result (65,"A" in ASCII)
+`
+runtime = new NVM(code)
+runtime.runall();
 
-draw.update()
-function drawdisp() {
-
-    for (ix=0;ix<dispsize[0];ix++) {
-        for (iy=0;iy<dispsize[1];iy++) {
-            draw.dot(ix,iy,[(ix)%255,(iy)%255,(ix+iy)%255])
-        }
-    }
-    for (ix=50;ix<dispsize[0]-50;ix++) {
-        for (iy=50;iy<dispsize[1]-50;iy++) {
-            draw.dot(ix,iy,[200,200,200])
-        }
-    }
-
-    for (ix=0;ix<NeknajImg[0].length;ix+=0.25) {
-        for (iy=0;iy<NeknajImg.length;iy+=0.25) {
-            let mx = 100
-            let my = 100
-            imx = Math.floor(ix)
-            imy = Math.floor(iy)
-            if (NeknajImg[imy][imx]==1) {
-                draw.dot(ix*4+mx,iy*4+my,[0,0,0])
-            }
-        }
-    }
-
-
-    for (ix=0;ix<cursorImg[0].length;ix++) {
-        for (iy=0;iy<cursorImg.length;iy++) {
-            if (cursorImg[iy][ix]==1) {
-                draw.dot(ix+Mouse.loc[0],iy+Mouse.loc[1],[0,0,0])
-            }
-            else if (cursorImg[iy][ix]==2) {
-                draw.dot(ix+Mouse.loc[0],iy+Mouse.loc[1],[255,255,255])
-            }
-        }
-    }
-
-    draw.update()
-
-    setTimeout(drawdisp,50)
-
-}
-drawdisp()
-
-
-
+console.log(runtime.getBinOut());
+console.log(runtime.getOut());
